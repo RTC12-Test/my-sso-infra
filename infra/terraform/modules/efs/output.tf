815d9f5aab06ce3d1842df23414c7a0890d1f846
aravind15b <aravind15b@test.com>
2024-07-28 14:25:49 +0530

    fix

--- infra/terraform/modules/efs/output.tf
+++ infra/terraform/modules/efs/output.tf
@@ -2,8 +2,4 @@
 output "aws_efs_file_id" {
   value = aws_efs_file_system.efs.id
 }
-# Output of efs access point arn
-output "aws_access_point_arn" {
-  value = aws_efs_access_point.access_point.arn
-}
 
