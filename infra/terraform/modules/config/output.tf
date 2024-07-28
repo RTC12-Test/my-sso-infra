a52c8f608665d05508f0be6e4efb6a4f840359b7
aravind15b <aravind15b@test.com>
2024-07-28 17:54:51 +0530

    fasd

--- infra/terraform/modules/config/output.tf
+++ infra/terraform/modules/config/output.tf
@@ -13,9 +13,3 @@ output "secrets" {
 output "taskdefintionfile" {
   value = "${path.module}/task-definition-json.tpl"
 }
-output "sg_comman" {
-  value = var.sg_comman
-}
-output "efs_id" {
-  value = var.efs_id
-}
--- infra/terraform/output.tf
+++ infra/terraform/output.tf
@@ -2,3 +2,6 @@
 output "aws_ecs_cluster_name" {
   value = module.ecs-cluster.aws_ecs_cluster_name
 }
+output "efs_id" {
+  value = module.config.efs_id
+}
