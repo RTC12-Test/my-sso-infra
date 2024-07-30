f9525dbdf5b78874cedbd425390f6f1525bc7d9a
aravind15b <aravind15b@test.com>
2024-07-30 14:23:53 +0530

    feat: testing falcon

--- infra/terraform/modules/task-definition/main.tf
+++ infra/terraform/modules/task-definition/main.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "task_definition" {
   memory                   = var.aws_ecs_task_definition_memory
   execution_role_arn       = var.aws_task_execution_role
   task_role_arn            = var.aws_task_execution_role
-  container_definitions    = templatefile(var.task_definition_file, var.task_definition_variables)
+  container_definitions    = templatefile(var.task_definition_file, local.task_definition_vars)
   dynamic "volume" {
     for_each = var.task_definition_variables.volumes
     content {
