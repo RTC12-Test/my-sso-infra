4404a51cb3d293e74879cb3e91c2d6018fc16f5d
aravind15b <aravind15b@test.com>
2024-09-18 21:24:48 +0530

    feat: test

--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -10,7 +10,8 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_s3_object" "s3_object" {
   count  = true ? 1 : 0
   bucket = aws_s3_bucket.s3_bucket.id
-  key    = "dummy"
+  key    = "python"
+  source = "python.zip"
 }
 resource "aws_s3_bucket_versioning" "s3_versioning" {
   count  = true ? 1 : 0
