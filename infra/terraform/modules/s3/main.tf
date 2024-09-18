4b0e7d5d8182d2aa1006b61ec1809b72afdf477e
aravind15b <aravind15b@test.com>
2024-09-18 16:34:18 +0530

    feat: test

--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -1,9 +1,17 @@
 # Resource to create s3 bucket
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "${var.org_name}-${var.app_name}-${var.env}-${var.service_name}-bucket"
-
   tags = merge(var.default_tags, {
     Name         = "${var.org_name}-${var.app_name}-${var.env}-${var.service_name}-bucket"
+    map-migrated = var.map_migrated_tag
+  })
+}
+# Resource to create s3 bucket
+resource "aws_s3_object" "s3_bucket" {
+  bucket = "${var.org_name}-${var.app_name}-${var.env}-${var.service_name}-bucket-key"
+  key    = ""
+  tags = merge(var.default_tags, {
+    Name         = "${var.org_name}-${var.app_name}-${var.env}-${var.service_name}-bucket-key"
     map-migrated = var.map_migrated_tag
   })
 }
