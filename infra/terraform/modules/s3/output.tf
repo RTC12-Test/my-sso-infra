a2ef4de6316687ca881c70b6643c10a25e7d82b7
aravind15b <aravind15b@test.com>
2024-09-18 17:56:56 +0530

    feat: test

--- infra/terraform/modules/s3/output.tf
+++ infra/terraform/modules/s3/output.tf
@@ -14,5 +14,5 @@ output "aws_s3_bucket_arn" {
 }
 # Output of s3 bucket arn
 output "aws_s3_object_version_id" {
-  value = aws_s3_object.s3_object.version_id
+  value = aws_s3_object.s3_object[0].version_id
 }
