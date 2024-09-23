94ed45021bd02f8db2a5f26c6782ca515613a16e
aravind15b <aravind15b@test.com>
2024-09-23 21:48:03 +0530

    feat: test

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -61,7 +61,7 @@ resource "aws_lb_listener" "https" {
     for_each = var.aws_target_groups
     content {
       type             = var.aws_alb_routing_type
-      target_group_arn = var.enable_codeploy ? try(regex("green", aws_lb_target_group.alb_target_group[default_action.value.tg_name].name), "") ? "" : aws_lb_target_group.alb_target_group[default_action.value.tg_name] : aws_lb_target_group.alb_target_group[default_action.value.tg_name].arn
+      target_group_arn = var.enable_codeploy && try(length(regex("green", aws_lb_target_group.alb_target_group[default_action.value.tg_name].name)) > 0, false) ? null : aws_lb_target_group.alb_target_group[default_action.value.tg_name].arn
     }
 
   }
