4a6fc1b9789219b7736cdbc2702edfd3781795b3
aravind15b <aravind15b@test.com>
2024-10-01 17:05:00 +0530

    feat: test

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -72,7 +72,7 @@
 # Resource to create rules for HTTPS listener
 resource "aws_lb_listener_rule" "https_listener_rule" {
   listener_arn = var.aws_hp
-  priority     = var.aws_lb_https_listener_rule_priority
+  priority     = "1"
   action {
     type = var.aws_alb_routing_type
     forward {
