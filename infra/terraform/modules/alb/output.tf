8bbe0a4b236853ffa923458b62c46664ad61bb48
aravind15b <aravind15b@test.com>
2024-10-01 17:09:57 +0530

    feat: test

--- infra/terraform/modules/alb/output.tf
+++ infra/terraform/modules/alb/output.tf
@@ -1,9 +1,9 @@
-output "alb_name" {
-  value = { for i in var.aws_target_groups : i.tg_name => aws_lb_target_group.alb_target_group[i.tg_name].name }
-}
-output "alb_target_arn" {
-  value = { for i in var.aws_target_groups : i.tg_name => aws_lb_target_group.alb_target_group[i.tg_name].arn }
-}
-output "a" {
-  value = local.target_groups
-}
+# output "alb_name" {
+#   value = { for i in var.aws_target_groups : i.tg_name => aws_lb_target_group.alb_target_group[i.tg_name].name }
+# }
+# output "alb_target_arn" {
+#   value = { for i in var.aws_target_groups : i.tg_name => aws_lb_target_group.alb_target_group[i.tg_name].arn }
+# }
+# output "a" {
+#   value = local.target_groups
+# }
