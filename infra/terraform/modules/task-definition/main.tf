c4e2496a603171a075502c036d6b85050014368e
aravind15b <aravind15b@test.com>
2024-11-06 17:26:43 +0530

    feat: test

--- infra/terraform/modules/task-definition/main.tf
+++ infra/terraform/modules/task-definition/main.tf
@@ -8,7 +8,6 @@ resource "aws_ecs_task_definition" "task_definition" {
   execution_role_arn       = var.aws_task_execution_role
   task_role_arn            = var.aws_task_execution_role
   container_definitions    = templatefile(var.task_definition_file, local.task_definition_vars)
-  track_latest             = true
   dynamic "volume" {
     for_each = try(var.task_definition_variables.volumes.bind, {})
     content {
@@ -21,9 +20,14 @@ resource "aws_ecs_task_definition" "task_definition" {
       name = volume.value.name
       efs_volume_configuration {
         file_system_id     = volume.value.efs_id
-        root_directory     = volume.value.session_path != "" ? volume.value.session_path : var.session_path
+        root_directory     = try(volume.value.efs_access_point, []) != [] ? var.session_path : try(volume.value.session_path, var.session_path)
         transit_encryption = var.aws_taskdefinition_volume_encryption
-
+        dynamic "authorization_config" {
+          for_each = try([for i in volume.value.efs_access_point : i], [])
+          content {
+            access_point_id = authorization_config.value
+          }
+        }
       }
     }
   }
