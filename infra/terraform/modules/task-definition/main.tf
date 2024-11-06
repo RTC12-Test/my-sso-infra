c5cd8cd86152d575b63c3d0e11d6289aebc30abf
aravind15b <aravind15b@test.com>
2024-11-06 19:20:00 +0530

    feat: test

--- infra/terraform/modules/task-definition/main.tf
+++ infra/terraform/modules/task-definition/main.tf
@@ -36,6 +36,6 @@ resource "aws_ecs_task_definition" "task_definition" {
     map-migrated = var.map_migrated_tag
   })
   lifecycle {
-    ignore_changes = [volume[*].name]
+    ignore_changes = [volume.*.name]
   }
 }
